ebfb25cf068068e35e2f0251f61acf983ccbebb8
Vikash <vikashkisku@gmail.com>
2019-12-12 14:36:26 +0530

    Added Footer

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
  
-        <title>Vikash Kumar Kisku</title>
+        <title>Ugc-Net Score Calculator</title>
  
         <!-- CSS -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,600">
@@ -14,7 +14,19 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
         <link rel="stylesheet" href="static/css/style.css">
-    </head>
+    
+	<style>
+		.footer {
+		   position: fixed;
+		   left: 0;
+		   bottom: 0;
+		   width: 100%;
+		   background-color: #03a9f4;
+		   color: white;
+		   text-align: center;
+		}
+	</style>
+	</head>
  
     <body>
 	<div id="div">
@@ -42,7 +54,7 @@
 		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 		<div class="form-group">
 		  <label for="usr">Your Assessment Link</label>
-		  <input type="link" value="https://cdn.digialm.com//per/g28/pub/2083/touchstone/AssessmentQPHTMLMode1//2083O19256/2083O19256S5D66101/15755797078177669/JH0205201461_2083O19256S5D66101E1.html"  required class="form-control" name="assessment" id="assessment">
+		  <input type="link" required class="form-control" name="assessment" id="assessment">
 		</div>
 		<div class="form-group">
 			  <label for="sel1">Select Subject:</label>
@@ -58,7 +70,9 @@
 		</form>
 		
 		
-		
+		<div class="footer">
+		  <p>Copyright©2019-<?php echo date("Y");?></p>
+		</div>
 		<script src="static/js/jquery-3.4.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -66,6 +80,7 @@
 		<script>
 			
 		</script>
+		
     </body>
  
 </html>
